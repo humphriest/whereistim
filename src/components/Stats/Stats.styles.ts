5f34a108d93257c37683b9fdd33008f20cd08a17
Tim Humphries <tim@dataships.io>
2022-09-23 15:54:34 -0600

    Add in animation for beer glass being filled

--- src/components/Stats/Stats.styles.ts
+++ src/components/Stats/Stats.styles.ts
@@ -1,15 +1,15 @@
+import { motion } from "framer-motion";
 import styled, { keyframes } from "styled-components";
 
 export const MainContainer = styled.div`
   height: 100vh;
   width: 100%;
   display: flex;
-  justify-content: center;
-  /* align-items: center; */
   z-index: 999 !important;
   margin-top: 97vh;
   background-color: #0c141f;
   position: absolute;
+  flex-direction: column;
 `;
 
 const blink = keyframes`
@@ -23,15 +23,109 @@ const blink = keyframes`
   opacity:0
 }`;
 
+export const StatsTitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const TravelStatsTitle = styled.h1`
   font-size: 60px;
   position: relative;
-  margin-left: 10px;
+  margin: 20px 50px;
+  color: white;
+  text-align: center;
 
   &::after {
     content: "";
     margin-left: 5px;
     border-right: 2px solid white;
     animation: ${blink} 1.5s infinite ease;
+    animation-delay: 7.5s;
   }
+`;
+
+export const StatsMainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const StatsBox = styled(motion.div)`
+  border: 20px solid #f3f3f3;
+  border-radius: 15px;
+  height: 200px;
+  width: 200px;
+  background-color: #f3f3f3;
+  transform: rotate(90deg);
+
+  color: black;
+  font-size: 16px;
+  text-align: center;
+  font-size: 26px;
+
+  @media screen and (max-width: 760px) {
+    margin: 0;
+    width: 100px;
+    height: 100px;
+  }
+`;
+
+export const StatsContentContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 10%;
+  gap: 50px;
+
+  @media screen and (max-width: 760px) {
+    grid-template-columns: repeat(2, 2fr);
+    gap: 20px;
+  }
+`;
+export const BeerGlassContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+export const BorderBox = styled.div`
+  position: relative;
+  transition: all 1s;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 25px;
+    right: -20px;
+    width: 20px;
+    height: 60px;
+    border: 10px solid lightgray;
+    border-radius: 10px;
+  }
+`;
+
+export const Glass = styled.div`
+  position: relative;
+  width: 80px;
+  height: 120px;
+  border-top: 50px solid white;
+  background-color: #f3f3f3;
+  border-radius: 5px;
+  border: 10px solid lightgray;
+  border-top: none;
+  overflow: hidden;
+`;
+
+export const BeerGlassLiquid = styled(motion.div)`
+  width: inherit;
+  height: inherit;
+  background: black;
+  transform-origin: 50% 100%;
+  border-top: solid 15px white;
+`;
+
+export const BeerStats = styled.div`
+  padding: 20px;
+  text-align: center;
+  color: #f3f3f3;
+  position: absolute;
 `;
--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,20 +1,94 @@
+import {
+  useMotionValue,
+  useScroll,
+  useTransform,
+  animate,
+} from "framer-motion";
 import { NextPage } from "next";
 import { useEffect } from "react";
 import { typeWriter } from "utils/typewriter";
-import { MainContainer, TravelStatsTitle } from "./Stats.styles";
+import {
+  BeerGlassContainer,
+  BeerGlassLiquid,
+  BeerStats,
+  BorderBox,
+  Glass,
+  MainContainer,
+  StatsBox,
+  StatsContentContainer,
+  StatsMainContainer,
+  StatsTitleContainer,
+  TravelStatsTitle,
+} from "./Stats.styles";
 
+const STATS_TITLE = "MY TRAVEL STATS";
 const Stats: NextPage = () => {
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.4, 0.6, 0.8, 1],
+    [0, 0, 0, 0, 0, 1]
+  );
+  const opacity = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.4, 0.6, 1],
+    [0, 0, 0, 0, 1]
+  );
+  const beer = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.4, 0.6, 0.9, 0.925, 0.95, 0.975, 1],
+    [0, 0, 0, 0, 0, 0.25, 0.5, 0.75, 1]
+  );
+  const motionValue = useMotionValue(0);
+  const amountOfBeer = useTransform(motionValue, [0, 1], [0, 1]);
   useEffect(() => {
+    setTimeout(() => {
+      animate(motionValue, 1, {
+        duration: 1,
+        ease: "easeInOut",
+        onComplete: () => {
+          motionValue.set(1);
+        },
+      });
+      console.log(motionValue.get());
+    }, 1000);
+
     const travelTitleDocument = document.getElementById("stats-title");
-    console.log(travelTitleDocument);
-    if (travelTitleDocument) typeWriter("STATS HERE", travelTitleDocument);
+    if (travelTitleDocument) typeWriter(STATS_TITLE, travelTitleDocument);
   }, []);
 
   return (
     <MainContainer id="travel-stats">
-      <TravelStatsTitle id="stats-title" style={{ color: "white" }}>
-        Test
-      </TravelStatsTitle>
+      <StatsTitleContainer>
+        <TravelStatsTitle id="stats-title" />
+      </StatsTitleContainer>
+      <StatsMainContainer>
+        <StatsContentContainer>
+          <StatsBox style={{ scale, opacity }}>KMS TRAVELLED</StatsBox>
+          <StatsBox style={{ scale, opacity }}>AMOUNT OF TIME GONE</StatsBox>
+          <StatsBox style={{ scale, opacity }}>
+            NUMBER OF CITIES VISITED
+          </StatsBox>
+          <StatsBox style={{ scale, opacity }}>
+            AMOUNT OF SHITE TALKED
+            <BeerGlassContainer>
+              <BorderBox>
+                <Glass>
+                  <BeerGlassLiquid
+                    style={{
+                      scaleY: motionValue.get() === 1 ? beer : amountOfBeer,
+                    }}
+                  >
+                    <BeerStats>TOO MUCH</BeerStats>
+                  </BeerGlassLiquid>
+                </Glass>
+              </BorderBox>
+            </BeerGlassContainer>
+          </StatsBox>
+          <StatsBox style={{ scale, opacity }}>DISTANCE AWAY FROM YOU</StatsBox>
+          <StatsBox style={{ scale, opacity }}>NUMBER OF PINTS</StatsBox>
+        </StatsContentContainer>
+      </StatsMainContainer>
     </MainContainer>
   );
 };
